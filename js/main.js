cbc75b4acf293d1ea51c3a9c8185a8167eeb2799
shaiod <tltmdpffl@gmail.com>
2022-12-06 18:24:08 +0900

    port

--- js/main.js
+++ js/main.js
@@ -41,23 +41,7 @@ $(window).on("wheel", function(e){
 	$html.animate({scrollTop : posTop},800);
  
 });
-// var mHtml = $("html");
-// var page = 1;
 
-// mHtml.animate({scrollTop : 0},10);
-
-// $(window).on("wheel", function(e) {
-//   if(mHtml.is(":animated")) return;
-//   if(e.originalEvent.deltaY > 0) {
-//       if(page == 7) return;
-//       page++;
-//   } else if(e.originalEvent.deltaY < 0) {
-//       if(page == 1) return;
-//       page--;
-//   }
-//   var posTop =(page-1) * $(window).height();
-//   mHtml.animate({scrollTop : posTop},800);
-// })
 
 
 
@@ -77,15 +61,15 @@ setInterval(typing, 200)
 //메뉴 안보였다가 보이게 하기
 $(document).ready(function(){
 
-    var navHeight = $("#quick").height(); 
+    var asideHeight = $("#quick").height(); 
     //quick 의 높이를 구하기
 
     $(".quick").hide();
     //스크롤시 나타날 객체 미리 숨기기
 
     $(window).scroll(function(){  // 윈도우 스크롤 기능 작동
-        var rollIt = $(this).scrollTop() >= navHeight; 
-// 스크롤의 정도가 navHeight 의 값을 넘었을 때 == 윈도우 스크롤의 값이 navHeight 의 높이와 같거나 크다
+        var rollIt = $(this).scrollTop() >= asideHeight; 
+// 스크롤의 정도가 asideHeight 의 값을 넘었을 때 == 윈도우 스크롤의 값이 asideHeight 의 높이와 같거나 크다
 
 /*
 scrollTop 은 윈도우에서 스크롤의 위치가 가장 상위에 있다는 의미로 해석
@@ -93,7 +77,7 @@ scrollTop 은 윈도우에서 스크롤의 위치가 가장 상위에 있다는 
 */
 
     if(rollIt){ 
-		//윈도우 스크롤 기능의 값이 navHeight 의 높이와 같거나 크면
+		//윈도우 스크롤 기능의 값이 asideHeight 의 높이와 같거나 크면
             $(".quick").show().css({"position":"fixed"});
         }
         else{
@@ -161,4 +145,5 @@ $(document).on('click', '#modal_btn', function (e) {
 AOS.init({
   duration: 1000,
 })
+
 
